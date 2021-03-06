681b7dd0ae9050ede1776a1807419ce8bae69ffc
byl18 <65812792+byl18@users.noreply.github.com>
2021-03-06 19:26:55 +0800

    Add files via upload

---
+++ 扫雷/resource.h
@@ -0,0 +1,56 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 SaoLei.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_SAOLEITYPE                  129
+#define IDB_BITMINE                     130
+#define IDB_BITMINE_1                   131
+#define IDB_BITMINE_2                   132
+#define IDB_BITMINE_3                   133
+#define IDB_BITMINE_4                   134
+#define IDB_BITMINE_5                   135
+#define IDB_BITMINE_6                   136
+#define IDB_BITMINE_7                   137
+#define IDB_BITMINE_8                   138
+#define IDB_BITMINE_0                   139
+#define IDB_BITFLAG                     140
+#define IDB_UNCLICKED                   141
+#define IDB_VICTORY                     142
+#define IDB_FAIL                        143
+#define IDD_HIGHSCORE                   144
+#define IDB_BITMINE_9                   145
+#define IDB_BIT_0                       146
+#define IDD_SIZE                        149
+#define IDD_SETNAME                     150
+#define IDB_BITMAP1                     151
+#define IDC_FIRST_TIME                  1001
+#define IDC_FIRST_NAME                  1004
+#define IDC_L                           1007
+#define IDC_W                           1008
+#define IDC_N                           1010
+#define IDC_LENGTH                      1011
+#define IDC_WIDTH                       1012
+#define IDC_MINE                        1013
+#define IDC_WORD                        1014
+#define IDC_NAME                        1015
+#define IDC_STATIC_RANK                 1016
+#define IDC_SECOND_NAME                 1017
+#define IDC_SECOND_TIME                 1018
+#define IDC_THIRD_NAME                  1019
+#define IDC_THIRD_TIME                  1020
+#define IDC_BUTTON1                     1022
+#define AFX_ID_GAOFEN                   32771
+#define ID_RESTART                      32773
+#define ID_32776                        32776
+// Next default values for new objects
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        152
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
